c20f2838101853c746fe2b91329156b2de00386d
felixniicummings <felixcummingsjr@gmail.com>
2020-12-14 22:45:34 -0500

    Updates

---
+++ 01_CafeClassRepo/01_CafeClassRepo/MenuItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_CafeClassRepo
+{
+    //POCO
+    public class MenuItem
+    {
+        public int MenuItemNumber { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<string> ListOfIngredients { get; set; } = new List<string>();
+        public Double Price { get; set; }
+
+        public MenuItem() { }
+
+    }
+}
---
+++ 01_CafeClassRepo/01_CafeClassRepo/MenuItemRepo.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_CafeClassRepo
+{
+    public class MenuItemRepo
+    {
+        private List<MenuItem> _listOfMenuItems = new List<MenuItem>();
+
+        //Create
+        public void AddItemToList(MenuItem item)
+        {
+            _listOfMenuItems.Add(item);
+        }
+
+        //Read
+        public List<MenuItem> GetMenuItems()
+        {
+            return _listOfMenuItems;
+        }
+
+        //Update
+
+        //Delete
+        public bool RemoveItemFromList(int itemnumber)
+        {
+            MenuItem item = GetMenuItemByNumber(itemnumber);
+
+            if(item == null)
+            {
+                return false;
+            }
+
+            int initialCount = _listOfMenuItems.Count;
+            _listOfMenuItems.Remove(item);
+
+            if(initialCount > _listOfMenuItems.Count)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        //Helper Method
+        public MenuItem GetMenuItemByNumber(int itemnumber )
+        {
+            foreach (MenuItem item in _listOfMenuItems)
+            {
+                if(item.MenuItemNumber == itemnumber)
+                {
+                    return item;
+                }
+            }
+
+            return null;
+        }
+
+    }
+}
