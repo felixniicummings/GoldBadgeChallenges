7b81cc323e3815322b35afe08d0642e6ae4b1b7c
felixniicummings <felixcummingsjr@gmail.com>
2020-12-17 18:38:43 -0500

    Saving updates

--- 01_CafeClassRepo/01_CafeClassRepo/MenuItem.cs
+++ 01_CafeClassRepo/01_CafeClassRepo/MenuItem.cs
@@ -12,10 +12,22 @@ namespace _01_CafeClassRepo
         public int MenuItemNumber { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public List<string> ListOfIngredients { get; set; } = new List<string>();
+        //public List<string> ListOfIngredients { get; set; } = new List<string>();
         public Double Price { get; set; }
 
         public MenuItem() { }
+
+        //public MenuItem(int menuitemnumber, string name, string description, List<string> listofingredients, Double price)
+
+        public MenuItem(int menuitemnumber, string name, string description,Double price)
+        {
+            MenuItemNumber = menuitemnumber;
+            Name = name;
+            Description = description;
+            //ListOfIngredients = listofingredients;
+            Price = price;
+        }
+
 
     }
 }
