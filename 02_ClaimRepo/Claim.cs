d6003b5c7eec765db47f816ab97c74c8ce8d4dc5
felixniicummings <felixcummingsjr@gmail.com>
2020-12-17 22:05:39 -0500

    Updates

---
+++ 02_ClaimRepo/Claim.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_ClaimRepo
+{
+    public enum ClaimType
+    {
+        Car = 1,
+        Home,
+        Theft
+    }
+    //POCO
+    public class Claim
+    {
+        public int ClaimID { get; set; }
+        public ClaimType TypeOfClaim { get; set; }
+        public string Description { get; set; }
+        public Double ClaimAmount { get; set; }
+        public DateTime DateOfIncident { get; set; }
+        public DateTime DateOfClaim { get; set; }
+        public bool IsValid { get; set; }
+
+        public Claim() { }
+
+        public Claim(int claimid, ClaimType typeofclaim, string description, Double claimamount, DateTime dateofincident, DateTime dateofclaim, bool isvalid)
+        {
+            ClaimID = claimid;
+            TypeOfClaim = typeofclaim;
+            Description = description;
+            ClaimAmount = claimamount;
+            DateOfIncident = dateofincident;
+            DateOfClaim = dateofclaim;
+            IsValid = isvalid;
+        }
+
+    }
+}
---
+++ 02_ClaimTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("02_ClaimTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("02_ClaimTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("42f852dd-395c-43b3-be9a-8209a5275e04")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
